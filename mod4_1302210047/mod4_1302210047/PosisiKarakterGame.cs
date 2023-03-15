dcc7a352ce6343f8dfa099fe05bec1b3f4e00bfa
ephymeraladuare <rozankurniapratamaa@gmail.com>
2023-03-15 16:26:32 +0700

    Menambahkan class posisi karakter game

---
+++ mod4_1302210047/mod4_1302210047/PosisiKarakterGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_1302210047
+{
+    internal class PosisiKarakterGame
+    {
+    }
+}
