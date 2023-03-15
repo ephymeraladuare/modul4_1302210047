f5e191c78cf903f890f2ba5b6c9f602b659d5fc0
ephymeraladuare <rozankurniapratamaa@gmail.com>
2023-03-15 16:20:19 +0700

    menambahkan class kodebuah

---
+++ mod4_1302210047/mod4_1302210047/Kodebuah.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210047;
+
+internal class KodeBuah
+{
+    public enum buah
+    {
+        Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+    }
+    String[] arr = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+    public string getKode_Buah(buah namabuah) => arr[(int)namabuah];
+}
+
---
+++ mod4_1302210047/mod4_1302210047/Program.cs
@@ -0,0 +1,4 @@
+﻿using modul4_1302210047;
+
+KodeBuah kb = new KodeBuah();
+Console.WriteLine(kb.getKode_Buah(KodeBuah.buah.Apel));
